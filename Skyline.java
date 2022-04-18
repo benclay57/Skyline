bd04043926be1abf433e86916cd2ce0945710d17
Ben <49768587+benclay57@users.noreply.github.com>
2022-04-18 14:09:40 -0700

    Skyline intial commit

---
+++ Skyline.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+public class Skyline {
+    public static void main(String[] args){
+        try {
+            readFile();
+          } catch (FileNotFoundException e) {
+            // Do something with `e`
+          } 
+
+    }
+
+    public static void readFile() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("file.txt"));
+        // ...
+      }
+}
+
